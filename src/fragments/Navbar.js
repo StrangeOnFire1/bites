b045b546b6112c2bc260c9f75eb7271d098edb71
StrangeOnFire1 <131391144+StrangeOnFire1@users.noreply.github.com>
2023-05-17 17:34:20 +0530

    Update Navbar.js

--- src/fragments/Navbar.js
+++ src/fragments/Navbar.js
@@ -5,35 +5,35 @@ export default function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img id='logo' src={Logo} alt='logo' /></a>
+                    <a className="navbar-brand" href="/"><img id='logo' src={Logo} alt='logo' /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav  ms-auto mb-2 mb-lg-0 ">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Menu</a>
+                                <a className="nav-link" href="/">Menu</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Order Online</a>
+                                <a className="nav-link" href="/">Order Online</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Reviews</a>
+                                <a className="nav-link" href="/">Reviews</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About Us</a>
+                                <a className="nav-link" href="/">About Us</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Contacts</a>
+                                <a className="nav-link" href="/">Contacts</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#"><i id='nav-icon' className="fa-solid fa-bag-shopping"></i></a>
+                                <a className="nav-link" href="/"><i id='nav-icon' className="fa-solid fa-bag-shopping"></i></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#"><button id='nav-button'>Reserve Table</button></a>
+                                <a className="nav-link" href="/"><button id='nav-button'>Reserve Table</button></a>
                             </li>
 
                         </ul>
