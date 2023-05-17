813c16ea8ee7ed3c69db1e57e2f76793991c9f8c
StrangeOnFire1 <131391144+StrangeOnFire1@users.noreply.github.com>
2023-05-17 17:13:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './fragments/Footer';
+import Chefs from './fragments/Chefs';
+import Download from './fragments/Download';
+import Menu from './fragments/Menu';
+import Navbar from './fragments/Navbar';
+import PopularDishes from './fragments/PopularDishes';
+import Reservation from './fragments/Reservation';
+import Reviews from './fragments/Reviews';
+import Services from './fragments/Services';
+import TopSection from './fragments/TopSection';
+// import  { useState } from 'react';
+import ReactDOM from 'react-dom'
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <TopSection/>
+    <PopularDishes/>
+    <Services/>
+    <Menu/>
+    <Reservation/>
+    <Reviews/>
+    <Chefs/>
+    <Download/>
+    <Footer/>
+    </>
   );
 }
 
